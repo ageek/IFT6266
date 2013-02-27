ce1b327c3af60a219a200cb7c49f20ef77d65d0a
archambaultv <archambault.v@gmail.com>
2013-02-27 16:34:59 -0500

    To put keypoints on the data ... work in progess

---
+++ Code/Facekeypoints.py
@@ -0,0 +1,5 @@
+__author__ = 'Vincent Archambault-Bouffard'
+
+#The purpose of this module is to compute keypoints for all faces for the kaggle competition :
+    # Emotion and identity detection from face images.
+
