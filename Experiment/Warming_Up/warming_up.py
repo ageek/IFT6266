312d53e9fe51686054d9618e3ebd7adc23e3b206
Vincent <archambault.v@gmail.com>
2013-03-06 02:43:47 -0500

    First try at the competition. Just to test the setup

---
+++ Experiment/Warming_Up/warming_up.py
@@ -0,0 +1,16 @@
+__author__ = "Vincent Archambault - Bouffard"
+
+
+from pylearn2.config import yaml_parse
+from Code.CheckDatasetPath import checkDatasetPath
+
+# Make sure we have the path to the dataset
+checkDatasetPath()
+
+# Import yaml file that specifies the model to train
+with open("warming_up.yaml", "r") as f:
+    yamlCode = f.read()
+
+# Training the model
+train = yaml_parse.load(yamlCode)  # Creates the object from the yaml file
+train.main_loop() # Starts training
