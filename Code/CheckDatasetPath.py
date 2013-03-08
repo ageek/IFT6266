97a0f9b07cd0ea697bfdac8ddbc26e969866e60a
archambaultv <archambault.v@gmail.com>
2013-03-08 11:49:01 -0500

    Add more path to the Python path

--- Code/CheckDatasetPath.py
+++ Code/CheckDatasetPath.py
@@ -6,10 +6,31 @@ import os
 
 def checkDatasetPath():
     """
+    Check if the root folder of this project is in the python path
     Check if the contest dataset is in the python path.
+    Check also for Xavier Bouthillier
     """
-    if any("ContestDataset" in s for s in sys.path):
-        return
+    if not any("IFT6266" in s for s in sys.path):
+        # We insert the path
+        d = os.path.join(os.getenv("HOME"),  "Documents/IFT6266/IFT6266")
+        if os.path.isdir(d):
+            sys.path.append(d)
+        else:
+            d = os.path.join(os.getenv("HOME"),  "Documents/University/IFT6266/")
+            if os.path.isdir(d):
+                sys.path.append(d)
 
-    # We insert the path for Vincent personal computer
-    sys.path.append(os.path.join( os.getenv("HOME"),  "Documents/University/IFT6266/ContestDataset"))
+    if not any("ContestDataset" in s for s in sys.path):
+        # We insert the path
+        d = os.path.join(os.getenv("HOME"),  "Documents/University/IFT6266/ContestDataset")
+        sys.path.append(d)
+
+    if not any("ift6266kaggle/transform/" in s for s in sys.path):
+        # We insert the path
+        d = os.path.join(os.getenv("HOME"),  "Documents/IFT6266/ift6266kaggle/transform")
+        if os.path.isdir(d):
+            sys.path.append(d)
+        else:
+            d = os.path.join(os.getenv("HOME"),  "Documents/University/IFT6266/transform")
+            if os.path.isdir(d):
+                sys.path.append(d)
