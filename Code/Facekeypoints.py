5eb8ae43c3bfd248d70b166c2b3a1b070ab21bf9
archambaultv <archambault.v@gmail.com>
2013-03-09 10:53:46 -0500

    PEP 8

--- Code/Facekeypoints.py
+++ Code/Facekeypoints.py
@@ -68,7 +68,6 @@ def convertDataSetToImages(savePath=None, extension=None):
             break
 
 
-
 def runFaceTracker(imagePath, extension=None):
     if extension is None:
         extension = "png"
