6eb30be7476803307e93478b6defb60fa9a73b16
Vincent <archambault.v@gmail.com>
2013-03-17 16:38:00 -0400

    Add greyscale function

--- Experiment/Unsupervised/Kmeans/patches.py
+++ Experiment/Unsupervised/Kmeans/patches.py
@@ -27,3 +27,28 @@ def crop_patches_color(image, patchsize, keypoints=None):
                               k[1] - patchsize / 2:k[1] + patchsize / 2,
                               :]
     return patches
+
+def crop_patches_grayscale(image, patchsize, keypoints=None):
+    """
+    Crop patches of the specified size around the keypoints
+
+    keypoints can be :
+     iterable( (x,y), (x,y) ... ) -> List of the center of the patches
+     interger -> Number of patches to crop at random position
+     None -> 10 patches crop at random position
+
+    Returns the patches in a matrix with data row wise
+    """
+    # ToDo fix bug when patchsize is odd
+    patchHalfSize = patchsize // 2
+    if not hasattr(keypoints, '__iter__'):
+        if keypoints is None:
+            keypoints = 10
+        keypoints = zip(numpy.random.randint(patchHalfSize, image.shape[0] - patchHalfSize, keypoints),
+                        numpy.random.randint(patchHalfSize, image.shape[1] - patchHalfSize, keypoints))
+
+    patches = numpy.zeros((len(keypoints), patchsize, patchsize))
+    for i, k in enumerate(keypoints):
+        patches[i, :] = image[k[0] - patchHalfSize:k[0] + patchHalfSize,
+                              k[1] - patchHalfSize:k[1] + patchHalfSize]
+    return patches
