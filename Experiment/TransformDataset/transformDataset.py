7ee0bd2e02b7c0951f970faa8e00f6dfb5638691
Vincent <archambault.v@gmail.com>
2013-03-17 13:24:48 -0400

    New experiment

--- Experiment/TransformDataset/transformDataset.py
+++ Experiment/TransformDataset/transformDataset.py
@@ -1,13 +1,22 @@
 __author__ = 'Vincent Archambault-Bouffard'
 
+import sys
 from pylearn2.config import yaml_parse
+sys.path.append('/Users/Archi/Documents/University/IFT6266/')
+sys.path.append('/Users/Archi/Documents/University/IFT6266/IFT6266')
+sys.path.append('/Users/Archi/Documents/University/IFT6266/IFT6266/Code')
 from Code.CheckDatasetPath import checkDatasetPath
 
 # Make sure we have the path to the dataset
 checkDatasetPath()
+sys.path.append('/Users/Archi/Documents/University/IFT6266/ift6266kaggle/transform')
 
 # Import yaml file that specifies the model to train
+<<<<<<< HEAD
 with open("convNet_2layer_32kernel_0.5Momemtum.yaml", "r") as f:
+=======
+with open("convNet_standardization.yaml", "r") as f:
+>>>>>>> New experiment
     yamlCode = f.read()
 
 # Training the model
